b700e166b0a478499e3fa59a7740fc74877dee2f
Ed Schmalzle <ed@nerded.net>
2012-02-15 21:45:51 -0500

    calendar date parsing

---
+++ lib/gtfs/calendar_date.rb
@@ -0,0 +1,12 @@
+module GTFS
+  class CalendarDate
+    include GTFS::Model
+
+    required_attrs :service_id, :date, :exception_type
+    attr_accessor *attrs
+
+    def self.parse_calendar_dates(data)
+      return parse_models(data)
+    end
+  end
+end
---
+++ spec/gtfs/calendar_date_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe GTFS::CalendarDate do
+  describe 'CalendarDate.parse_calendar_dates' do
+    let(:header_line) {"service_id,date,exception_type\n"}
+    let(:invalid_header_line) {",date,\n"}
+    let(:valid_line) {"3,20110905,1\n"}
+    let(:invalid_line) {"3,,1\n"}
+
+    subject {GTFS::CalendarDate.parse_calendar_dates(source_text)}
+
+    include_examples 'models'
+  end
+end
