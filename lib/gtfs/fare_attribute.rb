ae93003e6273e6a620dfd024b48de062b78aa4f4
Ed Schmalzle <ed@nerded.net>
2013-03-21 16:30:11 -0400

    Remove 'fare_' prefix from fare_attribute

--- lib/gtfs/fare_attribute.rb
+++ lib/gtfs/fare_attribute.rb
@@ -2,14 +2,13 @@ module GTFS
   class FareAttribute
     include GTFS::Model
 
-    has_required_attrs :id, :price, :currency_type, :payment_method
+    has_required_attrs :fare_id, :price, :currency_type, :payment_method
     has_optional_attrs :transfer_duration, :transfers
     attr_accessor *attrs
 
     collection_name :fare_attributes
     required_file false
     uses_filename 'fare_attributes.txt'
-    column_prefix :fare_
 
     def self.parse_fare_attributes(data, options={})
       return parse_models(data, options)
