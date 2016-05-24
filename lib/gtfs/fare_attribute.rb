4e9b7272ad87c5371efbf2d99c379c636ab56c2b
Quincy Iheme <qiheme@gmail.com>
2016-05-24 10:55:41 -0400

    Update fare attribute model

--- lib/gtfs/fare_attribute.rb
+++ lib/gtfs/fare_attribute.rb
@@ -2,8 +2,8 @@ module GTFS
   class FareAttribute
     include GTFS::Model
 
-    has_required_attrs :fare_id, :price, :currency_type, :payment_method
-    has_optional_attrs :transfer_duration, :transfers
+    has_required_attrs :fare_id, :price, :currency_type, :payment_method, :transfers
+    has_optional_attrs :transfer_duration 
     attr_accessor *attrs
 
     collection_name :fare_attributes
