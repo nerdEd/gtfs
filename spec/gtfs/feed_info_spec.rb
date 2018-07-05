77e9478bbbaf2b912e58540ede0d0b8c4ccaf9b5
Edouard Maffert <edouard.maffert@af83.com>
2018-07-05 15:13:48 +0200

    Add specs to FeedInfo

--- spec/gtfs/feed_info_spec.rb
+++ spec/gtfs/feed_info_spec.rb
@@ -11,4 +11,47 @@ describe GTFS::FeedInfo do
 
     include_examples 'models'
   end
+
+  describe 'FeedInfo.generate_feed_infos' do
+    it "should produce the correct csv output" do
+      csv = GTFS::FeedInfo.generate_feed_infos do |feed_infos|
+        feed_infos << {
+          publisher_name: 'P',
+          publisher_url: 'http://feed.test/test',
+          lang: 'FR',
+          start_date: '01012018',
+          end_date: '02022018'
+        }
+      end
+      csv.should eq("feed_publisher_name,feed_publisher_url,feed_lang,feed_start_date,feed_end_date\n"+
+      "P,http://feed.test/test,FR,01012018,02022018\n")
+    end
+
+    it "should filter dynamically unused csv columns" do
+      csv = GTFS::FeedInfo.generate_feed_infos do |feed_infos|
+        feed_infos << {
+          publisher_name: 'P',
+          publisher_url: 'http://feed.test/test',
+          lang: 'FR',
+          start_date: '01012018',
+        }
+        feed_infos << {
+          publisher_name: 'P',
+          publisher_url: 'http://feed.test/test',
+          lang: 'FR',
+        }
+        feed_infos << {
+          publisher_name: 'P',
+          publisher_url: 'http://feed.test/test',
+          lang: 'FR',
+          start_date: '01012018',
+        }
+      end
+      csv.should eq("feed_publisher_name,feed_publisher_url,feed_lang,feed_start_date\n"+
+      "P,http://feed.test/test,FR,01012018\n"+
+      "P,http://feed.test/test,FR,\n"+
+      "P,http://feed.test/test,FR,01012018\n"
+      )
+    end
+  end
 end
