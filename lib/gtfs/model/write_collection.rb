b4a98abfd972dfa5ffd4999b8206118af0f64ee4
Aurelien-af83 <aurelien.guiramand@af83.com>
2018-07-06 12:32:45 +0200

    changes according to review

--- lib/gtfs/model/write_collection.rb
+++ lib/gtfs/model/write_collection.rb
@@ -2,7 +2,7 @@ module GTFS
   module Model
     class WriteCollection
       def initialize(klass)
-        @obects_array = []
+        @objects_array = []
         @klass = klass
         @unused_attrs = @klass.optional_attrs_objects.dup
       end
@@ -10,14 +10,14 @@ module GTFS
       def push(data)
         object = @klass.new(data)
         @unused_attrs.delete_if {|a| !object.send(a.name).nil? }
-        @obects_array << object
+        @objects_array << object
       end
       alias_method :<<, :push
 
       def array_to_csv(csv)
-        return if @obects_array.empty?
+        return if @objects_array.empty?
         csv << @klass.csv_attrs - @unused_attrs.map(&:csv_name)
-        @obects_array.each {|o| csv << o.to_csv(@klass.attrs - @unused_attrs.map(&:name))}
+        @objects_array.each {|o| csv << o.to_csv(@klass.attrs - @unused_attrs.map(&:name))}
       end
     end
   end
